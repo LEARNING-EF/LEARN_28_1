d0d9c660f4b1af5a37a3441574436275cd9e42a8
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-11-01 13:06:52 +0330

    Add project files.

---
+++ Models/DatabaseContextInitializerBeforeTheFirstRelease.cs
@@ -0,0 +1,27 @@
+﻿namespace Models
+{
+	internal class DatabaseContextInitializerBeforeTheFirstRelease :
+		System.Data.Entity.DropCreateDatabaseIfModelChanges<DatabaseContext>
+	{
+		public DatabaseContextInitializerBeforeTheFirstRelease() : base()
+		{
+		}
+
+		protected override void Seed(DatabaseContext databaseContext)
+		{
+			//base.Seed(databaseContext);
+
+			try
+			{
+				DatabaseContextInitializer.Seed(databaseContext);
+			}
+			catch
+			{
+			}
+			//catch (System.Exception ex)
+			//{
+			//	Dtx.LogHandler.Report(GetType(), null, ex);
+			//}
+		}
+	}
+}
